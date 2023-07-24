2930dfb87dd35985e451c554104c0b2f99622254
Chinmay Takkar <95758118+Chinmay-Takkar@users.noreply.github.com>
2023-07-24 10:08:44 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -7,7 +7,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink = "https://raw.githubusercontent.com/Chinmay-Takkar/Portfolio/main/src/components/Resume/ResumeNew.js";
+const resumeLink = "https://raw.githubusercontent.com/Chinmay-Takkar/Resume/main/Resume_ChinmayTakkar.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
